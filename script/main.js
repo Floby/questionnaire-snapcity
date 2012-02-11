feba0e708945734c013a5d4ad8bd4db11815ad80
Florent Jaby <florent.jaby@gmail.com>
2012-02-11 16:34:40 +0100

    add some shiny effect

--- script/main.js
+++ script/main.js
@@ -1,5 +1,18 @@
 $(function() {
     $('#toggle-spread').click(function() {
-        $('.picture-collection-container').toggleClass('spread-4');
+        $('.picture-collection-container').toggleClass('spread-4');;
+    });
+
+    $('input[type=radio]').change(function() {
+        var id = this.id;
+        var name = this.name;
+        var form = this.form;
+        var checked = this.checked;
+        var all = $('input[name='+name+']', form);
+        all.each(function() {
+            var id = this.id;
+            $('label[for='+id+']', form).removeClass('active');
+        });
+        $('label[for='+id+']', form).addClass('active');
     })
 })
